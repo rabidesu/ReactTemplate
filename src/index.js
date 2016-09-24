94a7e781acc2dde51084f371a7e1987799b3fe5c
Rabidesu <saska0202@mail.ru>
2016-09-24 23:41:18 +0300

    template fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import { Router, Route, hashHistory} from 'react-router';
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path='/' component={App} >
-            <Route path="/about" component={AboutPage} />
-            <Route path="/inbox" component={InboxPage} >
-                <Route path="/inbox/message/:messageId" component={Message} />
-            </Route>
         </Route>
     </Router>,
     document.getElementById('mount-point')
